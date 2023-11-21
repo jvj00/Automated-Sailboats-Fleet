574f5ff9a06495c647e08329bc6140d2d76d9980
p3zz <federico.pezzato.work@gmail.com>
2023-11-21 14:38:57 +0100

    update customPrint

--- src/customPrint.py
+++ src/customPrint.py
@@ -1,33 +1,32 @@
-import datetime
+from utils import now
 
 class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    ORANGE = '\033[33m'
-    RESULT =  '\033[94m'
-    WARNING = '\033[93m'
-    DEBUGGING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        ORANGE = '\033[33m'
+        RESULT =  '\033[94m'
+        WARNING = '\033[93m'
+        DEBUGGING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
-def now():
-    return datetime.datetime.now().strftime("%H:%M:%S")
+class Logger:
 
-def error(buffer):
-    custom_print("ERROR", buffer, colors.FAIL)
-    
-def warning(buffer):
-    custom_print("WARNING", buffer, colors.ORANGE)
+    def error(buffer):
+        custom_print("ERROR", buffer, colors.FAIL)
+        
+    def warning(buffer):
+        custom_print("WARNING", buffer, colors.ORANGE)
 
-def debug(buffer):
-    custom_print("DEBUG", buffer, colors.DEBUGGING)
-    
-def info(buffer):
-    custom_print("INFO", buffer, colors.RESULT)
+    def debug(buffer):
+        custom_print("DEBUG", buffer, colors.DEBUGGING)
+        
+    def info(buffer):
+        custom_print("INFO", buffer, colors.RESULT)
 
 def custom_print(label, buffer, color):
     date = now()
@@ -35,7 +34,7 @@ def custom_print(label, buffer, color):
     print(f'{date} {prefix} {str(buffer)}\n')
 
 if __name__ == "__main__":
-    error("main")
-    warning("warning")
-    debug("debug")
-    info("info")
+    Logger.error("main")
+    Logger.warning("warning")
+    Logger.debug("debug")
+    Logger.info("info")
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 # generate random Gaussian values
 from random import seed
 from random import gauss
+from datetime import datetime
 
 def value_from_gaussian(mu, sigma):
     return gauss(mu, sigma)
+
+def now():
+    return datetime.now().strftime("%H:%M:%S")
