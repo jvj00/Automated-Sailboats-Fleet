3629dd36901ebc47af641483e1cfe13441c18b6a
p3zz <federico.pezzato.work@gmail.com>
2023-11-21 11:09:45 +0100

    fix sensor plot

--- src/Sensor.py
+++ src/Sensor.py
@@ -5,18 +5,21 @@ class Sensor:
     def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.sensor_data = pd.DataFrame()
+        self.sensor_data = {}
     
     def add_data(self, time, data):
         self.sensor_data[time] = data
     
     def plot(self):
-        plt.plot(self.sensor_data)
+        sorted_sensor_data = dict(sorted(self.sensor_data.items()))
+        x = sorted_sensor_data.keys()
+        y = sorted_sensor_data.values()
+        plt.plot(x, y)
         plt.show()
 
 if __name__ == "__main__":
     sensor = Sensor("test", 0)
     sensor.add_data(0, 3)
-    sensor.add_data(1, 2)
+    sensor.add_data(4, 2)
     sensor.add_data(2, 9)
     sensor.plot()
