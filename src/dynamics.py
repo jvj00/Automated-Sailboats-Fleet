c865ce0f8cbf2d3191d5048170c944b9d3740a64
p3zz <federico.pezzato.work@gmail.com>
2023-11-28 11:25:54 +0100

    refactor dynamics

--- src/dynamics.py
+++ src/dynamics.py
@@ -2,15 +2,13 @@ from customprint import Logger
 import numpy as np
 import matplotlib.pyplot as plt
 
-global world
-global velocities
-global positions
-global times
-
 class Wind:
     def __init__(self):
         self.density = 1.293
         self.velocity = np.zeros(3)
+    
+    def get_angle(self):
+        return np.arctan2(self.velocity[1], self.velocity[0])
 
 class Boat:
     def __init__(self):
@@ -25,6 +23,24 @@ class World:
         self.gravity = np.array([0.0, 0.0, 9.81])
         self.wind = wind
         self.boat = boat
+    
+    def update(self, dt):
+        boat_weight = self.gravity * self.boat.mass
+
+        # boat/water friction setup
+        friction_force_perp = self.boat.friction * boat_weight
+        
+        wind_force = compute_wind_force(self.wind.density, self.wind.velocity, self.boat.wing_area)
+        wind_angle = self.wind.get_angle()
+        friction_force_x = friction_force_perp[2] * np.cos(wind_angle)
+        friction_force_y = friction_force_perp[2] * np.sin(wind_angle)
+        friction_force = np.array([friction_force_x, friction_force_y, 0.0])
+
+        acceleration = compute_acceleration(wind_force - friction_force, self.boat.mass)
+
+        self.boat.velocity = compute_velocity(acceleration, self.boat.velocity, dt)
+        self.boat.position = compute_position(acceleration, self.boat.velocity, self.boat.position, dt)
+
 
 def compute_magnitude(vec):
     return np.sqrt(np.sum([c*c for c in vec]))
@@ -48,33 +64,9 @@ def compute_wind_force(air_density: float, wind_velocity, wing_area: float):
     air_mass = air_density * wing_area
     return air_mass * wind_velocity
 
-def process(dt: float):
-    
-    boat_weight = world.gravity * world.boat.mass
-
-    # boat/water friction setup
-    friction_force_perp = world.boat.friction * boat_weight
-    
-    wind_force = compute_wind_force(world.wind.density, world.wind.velocity, world.boat.wing_area)
-    wind_angle = np.arctan2(world.wind.velocity[1], world.wind.velocity[0])
-
-    friction_force_x = friction_force_perp[2] * np.cos(wind_angle)
-    friction_force_y = friction_force_perp[2] * np.sin(wind_angle)
-    friction_force = np.array([friction_force_x, friction_force_y, 0.0])
-
-    acceleration = compute_acceleration(wind_force - friction_force, world.boat.mass)
-
-    world.boat.velocity = compute_velocity(acceleration, world.boat.velocity, dt)
-    world.boat.position = compute_position(acceleration, world.boat.velocity, world.boat.position, dt)
-
-    velocities.append(compute_magnitude(world.boat.velocity))
-    positions.append(compute_magnitude(world.boat.position))
-    times.append(time_elapsed)
-
-    Logger.debug(f'Velocity: {world.boat.velocity}, Position: {world.boat.position}')
-
 if __name__ == '__main__':
     wind = Wind()
+    wind.velocity = np.array([13.0, 11.0, 0.0])
     boat = Boat()
     world = World(wind, boat)
 
@@ -85,7 +77,11 @@ if __name__ == '__main__':
     dt = 0.5
     
     for time_elapsed in np.arange(0, 100, dt):
-        process(dt)
-            
-    plt.plot(times, velocities)
+        world.update(dt)
+        velocities.append(world.boat.velocity)
+        positions.append(world.boat.position)
+        times.append(time_elapsed)
+
+    y = list(map(lambda p: compute_magnitude(p), velocities))
+    plt.plot(times, y)
     plt.show()
