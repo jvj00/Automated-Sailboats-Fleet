25bdc8fb78866a312c7df7cf21c9f291e07d6f0b
p3zz <federico.pezzato.work@gmail.com>
2023-12-03 17:46:41 +0100

    turn 3d vectors into 2d

--- src/dynamics.py
+++ src/dynamics.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 class Wind:
     def __init__(self):
         self.density = 1.293
-        self.velocity = np.zeros(3)
-    
-    def get_angle(self):
-        return np.arctan2(self.velocity[1], self.velocity[0])
+        self.velocity = np.zeros(2)
+
+class Wing:
+    def __init__(self, heading):
+        self.area = 15
+        # heading is perpendicular to the surface of the wing, pointing forward
+        self.heading = heading
 
 class Boat:
     def __init__(self):
-        self.wing_area = 15
         self.mass = 100
-        self.position = np.zeros(3)
-        self.velocity = np.zeros(3)
-        self.acceleration = np.zeros(3)
+        self.position = np.zeros(2)
+        self.velocity = np.zeros(2)
+        self.acceleration = np.zeros(2)
+        self.heading = np.zeros(2)
+        self.wing = Wing(self.heading)
 
 # m_velocity -= m_velocity * m_linearDrag * a_timeStep;
 
@@ -33,28 +37,31 @@ class Boat:
 
 class World:
     def __init__(self, wind: Wind, boat: Boat):
-        self.gravity = np.array([0, 0, 9.81])
+        self.gravity = 9.81
         self.wind = wind
         self.boat = boat
     
     def update(self, dt):
         # apply friction to the boat
         boat_weight = self.boat.mass * self.gravity
-        damping_factor = 10 / boat_weight[2]
+        damping_factor = 10 / boat_weight
         self.boat.velocity -= self.boat.velocity * damping_factor * dt
 
         if compute_magnitude(self.boat.velocity) < 0.01:
-            self.boat.velocity = np.zeros(3)
+            self.boat.velocity = np.zeros(2)
         
         # apply wind force to the boat
-        wind_force = compute_wind_force(self.wind.density, self.wind.velocity, self.boat.wing_area)
+        wind_force = compute_wind_force(self.wind.density, self.wind.velocity, self.boat.wing.area)
 
         self.boat.acceleration = compute_acceleration(wind_force, self.boat.mass)
         self.boat.velocity += (self.boat.acceleration * dt)
         self.boat.position += (self.boat.velocity * dt)
 
+def compute_angle(vec):
+    return np.arctan2(vec[1], vec[0])
+
 def compute_magnitude(vec):
-    return np.sqrt(np.sum([c*c for c in vec]))
+    return np.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
 
 def compute_acceleration(force, mass):
     return force / mass
@@ -75,15 +82,13 @@ if __name__ == '__main__':
     positions = []
     times = []
 
-    MAX_SPEED = 20
-
     dt = 0.5
 
-    world.wind.velocity = np.array([25.0, 25.0, 0.0])
+    world.wind.velocity = np.array([15.0, 15.0])
 
     for time_elapsed in np.arange(0, 100, dt):
         if time_elapsed % 5 == 0 and  0 < time_elapsed < 10:
-            world.wind.velocity = np.zeros(3)
+            world.wind.velocity = np.zeros(2)
         velocities.append(world.boat.velocity.copy())
         positions.append(world.boat.position.copy())
         times.append(time_elapsed)
