fix boat rotation using rudder

src/entities.py
@@ -27,18 +27,29 @@ class Boat:
         self.wing = wing
         self.rudder = rudder
         self.damping = 0.005
+        self.angular_damping = 0.05
     
     def position_matrix(self):
         return np.array([*self.position, compute_angle(self.heading)])
     
+    # compute the rotation rate
+    # the rotation rate is directly proportional to the rudder angle and the boat velocity.
+    # the higher is the rudder angle and the boat velocity, the higher will be the rotation rate of the boat
+    # the result is scaled is using an angular damping
+    def rotate(self, dt):
+        rotation_rate = self.rudder.get_heading() * self.angular_damping
+        self.heading += rotation_rate * compute_magnitude(self.velocity) * dt
+    
+    def translate(self, dt):
+        self.velocity += (self.acceleration * dt)
+        self.position += (self.velocity * dt)
+
     def move(self, dt):
         self.rudder.move(dt)
         self.wing.move(dt)
-        self.heading = self.rudder.get_heading()
-        self.acceleration = np.dot(self.acceleration, self.heading) * self.heading
-        self.velocity += (self.acceleration * dt)
-        self.position += (self.velocity * dt)
-    
+        self.rotate(dt)
+        self.translate(dt)
+        
     def apply_wind(self, wind: Wind):
         wind_force = compute_wind_force(wind, self)
         self.acceleration = compute_acceleration(wind_force, self.mass)

src/main.py
@@ -9,7 +9,7 @@ from logger import Logger
 
 if __name__ == '__main__':
     wind = Wind(1.291)
-    boat = Boat(100, Wing(15, Stepper(100, 0.02)), Rudder(Stepper(100, 0.05)))
+    boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
     anemo = Anemometer(0.5)
     world = World(9.81, wind, boat)
 
@@ -27,18 +27,18 @@ if __name__ == '__main__':
 
     # spawn the boat in the center of the map
     world.boat.position = np.array([width * 0.5, height * 0.5])
-    world.boat.heading = np.array([0, -1])
-    world.wind.velocity = np.array([-10, 15])
-    world.boat.rudder.set_target(np.pi * 0.5)
+    world.boat.heading = np.array([0.0, -1.0])
+    world.wind.velocity = np.array([-10.0, 15.0])
+    world.boat.rudder.set_target(np.pi * 0.25)
     world.boat.wing.set_target(np.pi)
     
     wing_angle = np.pi * 0.2
 
-    for time_elapsed in np.arange(0, 1000, dt):
+    for time_elapsed in np.arange(0, 500, dt):
         if time_elapsed == 20:
             world.wind.velocity = np.zeros(2)
-        if time_elapsed == 10:
-            world.boat.rudder.set_target(np.pi)
+        # if time_elapsed == 10:
+        #     world.boat.rudder.set_target(np.pi)
         
         velocities.append(world.boat.velocity.copy())
         positions.append(world.boat.position.copy())
@@ -58,14 +58,14 @@ if __name__ == '__main__':
         drawer.draw_vector(world.boat.position, world.boat.velocity, 'green')
 
         #Plot anemometer measurements
-        plt.figure(1)
-        plt.cla()
-        plt.plot(times, anemo_truth, label="Anemo truth")
-        plt.plot(times, anemo_meas, label="Anemo meas")
+        # plt.figure(1)
+        # plt.cla()
+        # plt.plot(times, anemo_truth, label="Anemo truth")
+        # plt.plot(times, anemo_meas, label="Anemo meas")
         # plt.plot(times, list(map(lambda p: p[0], velocities)), label='Boat Velocity X')
         # plt.plot(times, list(map(lambda p: p[1], velocities)), label='Boat Velocity Y')
 
         # plt.legend()
         plt.pause(dt)
     
-    plt.show()
+    # plt.show()