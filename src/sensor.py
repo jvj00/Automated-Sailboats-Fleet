91ff9d09eba06c94ed513450b551f2612c6c605e
JvJ00 <manuel.righele@studenti.unitn.it>
2023-12-16 15:46:00 +0100

    Added all used sensors and their tests

--- src/sensor.py
+++ src/sensor.py
@@ -1,29 +1,36 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 from logger import Logger
 from utils import value_from_gaussian
-from entities import Boat, Wind, World
+from entities import Boat, Wind
 from utils import *
 
 class Error:
     def __init__(self):
         pass
-    def get_sigma(self, value):
+    def get_sigma(self, value): # consider 3 sigma rule: encapsulate 99.7% of the values in 3 sigma
         pass
 
 class AbsoluteError(Error):
     def __init__(self, error):
         self.error = error
     def get_sigma(self, value):
-        return self.error/3.0
+        return self.error /3.0
 
 class RelativeError(Error):
     def __init__(self, error):
         self.error = error
     def get_sigma(self, value):
-        return (self.error*value)/3.0
+        return (self.error*value) /3.0
 
+class MixedError(Error):
+    def __init__(self, error, threshold):
+        self.error = error
+        self.threshold = threshold
+    def get_sigma(self, value):
+        return self.error * (self.threshold if value < self.threshold else value) /3.0
+
+# JRC WS-12 (set velocity error RELATIVE to 5% and direction error ABSOLUTE to 1*pi/180 rad)
 class Anemometer:
     def __init__(self, err_velocity: Error, err_direction: Error):
         self.err_velocity = err_velocity
@@ -39,7 +46,49 @@ class Anemometer:
         measured = np.array([value_from_gaussian(wind_vel_anemo, self.err_velocity.get_sigma(wind_vel_anemo)), value_from_gaussian(wind_dir_anemo, self.err_direction.get_sigma(wind_dir_anemo))])
         return truth, measured
 
+# DX900+ (set velocity error MIXED with threshold of 5m/s and 1% of error)
+class Speedometer:
+    def __init__(self, err_velocity: Error):
+        self.err_velocity = err_velocity
+    
+    def measure(self, boat: Boat):
+        boat_mag, boat_angle = cartesian_to_polar(boat.velocity)
+        truth = boat_mag
+        measured = value_from_gaussian(boat_mag, self.err_velocity.get_sigma(boat_mag))
+        return truth, measured
 
+# HSC100 (set direction error ABSOLUTE to 3*pi/180 rad)
+class Compass:
+    def __init__(self, err_direction: Error):
+        self.err_direction = err_direction
+    
+    def measure(self, boat: Boat):
+        truth = compute_angle(boat.heading)
+        measured = value_from_gaussian(truth, self.err_direction.get_sigma(truth))
+        return truth, measured
+
+# DW3000 (set distance error ABSOLUTE to 0.2m) (TWR)
+class UWB:
+    def __init__(self, err_distance: Error):
+        self.err_distance = err_distance
+    
+    def measure(self, b_actual: Boat, b_target: Boat):
+        truth = compute_magnitude(b_actual.position - b_target.position)
+        measured = value_from_gaussian(truth, self.err_distance.get_sigma(truth))
+        return truth, measured
+
+# SAM-M10Q (set position error ABSOLUTE to 1.5m for both x and y)
+class GNSS:
+    def __init__(self, err_position_x: Error, err_position_y: Error):
+        self.err_position_x = err_position_x
+        self.err_position_y = err_position_y
+    
+    def measure(self, boat: Boat):
+        truth_x = boat.position[0]
+        truth_y = boat.position[1]
+        measured_x = value_from_gaussian(truth_x, self.err_position_x.get_sigma(truth_x))
+        measured_y = value_from_gaussian(truth_y, self.err_position_y.get_sigma(truth_y))
+        return np.array([truth_x, truth_y]), np.array([measured_x, measured_y])
 
 if __name__ == "__main__":
     from entities import Wing, Rudder, Stepper
---
+++ src/test_sensor.py
@@ -0,0 +1,146 @@
+import unittest
+import numpy as np
+from entities import Wing, Rudder, Stepper, Boat, Wind
+from sensor import Anemometer, Speedometer, Compass, UWB, GNSS, RelativeError, AbsoluteError, MixedError
+
+test_repetition = 10000
+
+class AnemometerTest(unittest.TestCase):
+
+    def test_outliers(self):
+        err_velocity = RelativeError(0.05)
+        err_direction = AbsoluteError(np.pi/180)
+        sensor = Anemometer(err_velocity=err_velocity, err_direction=err_direction)
+        boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        wind = Wind(1.291)
+        wind.velocity = np.array([np.random.rand()*20, np.random.rand()*20])
+        boat.velocity = np.array([np.random.rand()*20, np.random.rand()*20])
+        list_truth = []
+        list_meas = []
+        outliers_direction = 0
+        outliers_velocity = 0
+        
+        for i in range(test_repetition):
+            truth, meas = sensor.measure(wind, boat)
+            if np.abs(meas[0] - truth[0]) > np.abs(err_velocity.error * truth[0]):
+                outliers_velocity += 1
+            if np.abs(meas[1] - truth[1]) > err_direction.error:
+                outliers_direction += 1
+            list_truth.append(truth)
+            list_meas.append(meas)
+
+        self.assertLess(outliers_velocity/test_repetition, 0.004)
+        self.assertLess(outliers_direction/test_repetition, 0.004)
+
+class SpeedometerTest(unittest.TestCase):
+
+    def test_outliers_high_velocities(self):
+        err_velocity = MixedError(0.01, 5)
+        sensor = Speedometer(err_velocity=err_velocity)
+        boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        x=np.random.rand()*15+4
+        y=np.random.rand()*15+4
+        boat.velocity = np.array([np.random.rand()*15+5, np.random.rand()*15+5])
+        list_truth = []
+        list_meas = []
+        outliers_velocity = 0
+
+        for i in range(test_repetition):
+            truth, meas = sensor.measure(boat)
+            if np.abs(meas - truth) > np.abs(err_velocity.error * truth):
+                outliers_velocity += 1
+            list_truth.append(truth)
+            list_meas.append(meas)
+
+        self.assertLess(outliers_velocity/test_repetition, 0.004)
+
+    def test_outliers_low_velocities(self):
+        err_velocity = MixedError(0.01, 5)
+        sensor = Speedometer(err_velocity=err_velocity)
+        boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        x=np.random.rand()*5
+        y=np.random.rand()*np.sqrt(25-x**2)
+        boat.velocity = np.array([x, y])
+        list_truth = []
+        list_meas = []
+        outliers_velocity = 0
+
+        for i in range(test_repetition):
+            truth, meas = sensor.measure(boat)
+            if np.abs(meas - truth) > np.abs(err_velocity.error * err_velocity.threshold):
+                outliers_velocity += 1
+            list_truth.append(truth)
+            list_meas.append(meas)
+
+        self.assertLess(outliers_velocity/test_repetition, 0.004)
+
+class CompassTest:
+    
+    def test_outliers(self):
+        err_direction = AbsoluteError(3*np.pi/180)
+        sensor = Compass(err_direction=err_direction)
+        boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        boat.heading = np.array([np.random.rand(), np.random.rand()])
+        list_truth = []
+        list_meas = []
+        outliers_direction = 0
+        
+        for i in range(test_repetition):
+            truth, meas = sensor.measure(boat)
+            if np.abs(meas - truth) > err_direction.error:
+                outliers_direction += 1
+            list_truth.append(truth)
+            list_meas.append(meas)
+
+        self.assertLess(outliers_direction/test_repetition, 0.004)
+
+class UWBSensorTest(unittest.TestCase):
+
+    def test_outliers(self):
+        err_distance = AbsoluteError(0.2)
+        sensor = UWB(err_distance=err_distance)
+        boat_actual = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        boat_target = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+        boat_actual.position = np.array([np.random.rand()*100, np.random.rand()*100])
+        boat_target.position = np.array([np.random.rand()*100, np.random.rand()*100])
+        list_truth = []
+        list_meas = []
+        outliers_distance = 0
+        
+        for i in range(test_repetition):
+            truth, meas = sensor.measure(boat_actual, boat_target)
+            if np.abs(meas - truth) > err_distance.error:
+                outliers_distance += 1
+            list_truth.append(truth)
+            list_meas.append(meas)
+
+        self.assertLess(outliers_distance/test_repetition, 0.004)
+
+class GNSSSensorTest(unittest.TestCase):
+    
+        def test_outliers(self):
+            err_position_x = AbsoluteError(1.5)
+            err_position_y = AbsoluteError(1.5)
+            sensor = GNSS(err_position_x=err_position_x, err_position_y=err_position_y)
+            boat = Boat(100, Wing(15, Stepper(100, 0.05)), Rudder(Stepper(100, 0.2)))
+            boat.position = np.array([np.random.rand()*100, np.random.rand()*100])
+            list_truth = []
+            list_meas = []
+            outliers_position_x = 0
+            outliers_position_y = 0
+            
+            for i in range(test_repetition):
+                truth, meas = sensor.measure(boat)
+                if np.abs(meas[0] - truth[0]) > err_position_x.error:
+                    outliers_position_x += 1
+                if np.abs(meas[1] - truth[1]) > err_position_y.error:
+                    outliers_position_y += 1
+                list_truth.append(truth)
+                list_meas.append(meas)
+    
+            self.assertLess(outliers_position_x/test_repetition, 0.004)
+            self.assertLess(outliers_position_y/test_repetition, 0.004)
+
+
+if __name__ == '__main__':
+    unittest.main()
