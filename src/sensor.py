8d693e7cf1b8a2852096fb13b48636913808983c
JvJ00 <manuel.righele@studenti.unitn.it>
2023-12-27 16:31:33 +0100

    Changed gerarchy (deleted import from boat and wind)

--- src/sensor.py
+++ src/sensor.py
@@ -3,7 +3,6 @@ import numpy as np
 from logger import Logger
 from pid import PID
 from utils import value_from_gaussian
-from entities import Boat, Wind
 from utils import *
 
 class Error:
@@ -38,45 +37,57 @@ class Anemometer:
         self.err_direction = err_direction
     
 
-    def measure(self, wind: Wind, boat: Boat):
-        wind_mag, wind_angle = cartesian_to_polar(wind.velocity)
-        boat_mag, boat_angle = cartesian_to_polar(boat.velocity)
+    def measure_with_truth(self, wind_velocity, boat_velocity):
+        wind_mag, wind_angle = cartesian_to_polar(wind_velocity)
+        boat_mag, boat_angle = cartesian_to_polar(boat_velocity)
         wind_vel_anemo = wind_mag - boat_mag * np.cos(mod2pi(wind_angle - boat_angle))
         wind_dir_anemo = mod2pi(wind_angle - boat_angle)
         truth = np.array([wind_vel_anemo, wind_dir_anemo])
         measured = np.array([value_from_gaussian(wind_vel_anemo, self.err_velocity.get_sigma(wind_vel_anemo)), value_from_gaussian(wind_dir_anemo, self.err_direction.get_sigma(wind_dir_anemo))])
         return truth, measured
 
+    def measure(self, wind_velocity, boat_velocity):
+        return self.measure_with_truth(wind_velocity, boat_velocity)[1]
+
 # DX900+ (set velocity error MIXED with threshold of 5m/s and 1% of error)
 class Speedometer:
     def __init__(self, err_velocity: Error):
         self.err_velocity = err_velocity
     
-    def measure(self, boat: Boat):
-        boat_mag, boat_angle = cartesian_to_polar(boat.velocity)
+    def measure_with_truth(self, boat_velocity):
+        boat_mag, boat_angle = cartesian_to_polar(boat_velocity)
         truth = boat_mag
         measured = value_from_gaussian(boat_mag, self.err_velocity.get_sigma(boat_mag))
         return truth, measured
+
+    def measure(self, boat_velocity):
+        return self.measure_with_truth(boat_velocity)[1]
 
 # HSC100 (set direction error ABSOLUTE to 3*pi/180 rad)
 class Compass:
     def __init__(self, err_direction: Error):
         self.err_direction = err_direction
     
-    def measure(self, boat: Boat):
-        truth = compute_angle(boat.heading)
+    def measure_with_truth(self, boat_heading):
+        truth = compute_angle(boat_heading)
         measured = value_from_gaussian(truth, self.err_direction.get_sigma(truth))
         return truth, measured
+
+    def measure(self, boat_heading):
+        return self.measure_with_truth(boat_heading)[1]
 
 # DW3000 (set distance error ABSOLUTE to 0.2m) (TWR)
 class UWB:
     def __init__(self, err_distance: Error):
         self.err_distance = err_distance
     
-    def measure(self, b_actual: Boat, b_target: Boat):
-        truth = compute_magnitude(b_actual.position - b_target.position)
+    def measure_with_truth(self, b_actual_position, b_target_position):
+        truth = compute_magnitude(b_actual_position - b_target_position)
         measured = value_from_gaussian(truth, self.err_distance.get_sigma(truth))
         return truth, measured
+    
+    def measure(self, b_actual_position, b_target_position):
+        return self.measure_with_truth(b_actual_position, b_target_position)[1]
 
 # SAM-M10Q (set position error ABSOLUTE to 1.5m for both x and y)
 class GNSS:
@@ -84,15 +95,19 @@ class GNSS:
         self.err_position_x = err_position_x
         self.err_position_y = err_position_y
     
-    def measure(self, boat: Boat):
-        truth_x = boat.position[0]
-        truth_y = boat.position[1]
+    def measure_with_truth(self, boat_position):
+        truth_x = boat_position[0]
+        truth_y = boat_position[1]
         measured_x = value_from_gaussian(truth_x, self.err_position_x.get_sigma(truth_x))
         measured_y = value_from_gaussian(truth_y, self.err_position_y.get_sigma(truth_y))
         return np.array([truth_x, truth_y]), np.array([measured_x, measured_y])
+    
+    def measure(self, boat_position):
+        return self.measure_with_truth(boat_position)[1]
+    
 
-if __name__ == "__main__":
-    from entities import Wing, Rudder, Stepper
+def test_sensor():
+    from entities import Wing, Rudder, Stepper, Boat, Wind
     test_repetition = 20
     err_velocity = RelativeError(0.05)
     err_direction = AbsoluteError(np.pi/180)
@@ -113,7 +128,7 @@ if __name__ == "__main__":
     for i in range(test_repetition):
         wind.velocity = np.array([20.0, 0.0])
         boat.velocity = np.array([5.0, 5.0])
-        truth, meas = sensor.measure(wind, boat)
+        truth, meas = sensor.measure(wind.velocity, boat.velocity)
         if np.abs(meas[0] - truth[0]) > err_velocity.error * truth[0]:
             outliers_velocity += 1
         if np.abs(meas[1] - truth[1]) > err_direction.error:
@@ -138,3 +153,6 @@ if __name__ == "__main__":
     plt.legend()
 
     plt.show()
+
+if __name__ == '__main__':
+    test_sensor()
