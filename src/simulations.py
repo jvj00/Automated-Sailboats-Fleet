40e1a2d6a8919a421b4a23e4d588f63287918dc0
p3zz <federico.pezzato.work@gmail.com>
2024-01-21 12:27:00 +0100

    turn simulations into test suite

--- src/simulations.py
+++ src/simulations.py
@@ -1,4 +1,5 @@
 # seadbed initialization
+import unittest
 from actuators import Motor, MotorController, Rudder, Stepper, StepperController, Wing
 from disegnino import Drawer
 from entities import Boat, Wind, World
@@ -9,13 +10,6 @@ import matplotlib.pyplot as plt
 from pid import PID
 from utils import compute_angle_between, compute_distance, mod2pi, modpi, polar_to_cartesian
 
-seabed = SeabedMap(0,0,0,0)
-## wind initialization
-wind = Wind(1.291)
-world = World(9.81, wind, seabed)
-
-# world initialization
-
 win_width = 900
 win_height = 500
 
@@ -24,134 +18,316 @@ world_height = 250
 drawer = Drawer(win_width, win_height, world_width, world_height)
 drawer.debug = True
 
-rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.25)
-wing_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0))
-motor_controller = MotorController(Motor(200, 0.85, 1024))
-
-## boat initialization
-boat = Boat(40, 10, Wing(15, wing_controller), Rudder(rudder_controller), motor_controller, seabed)
-
-def simulation_motor_only(targets):
+def simulate(boat: Boat, world: World, targets, dt, simulation_time, simulated_data = False, measured_data = False, motor_only = False):
+    route = []
 
     current_target_idx = 0
     target_radius = 5
-    route = []
-    time = []
-    rudder_angle = []
-    rudder_speed = []
-
-    boat.position = np.array([0.0, 0.0])
-    boat.velocity = np.array([0.0, 0.0])
-    boat.heading = polar_to_cartesian(1, 0)
-    boat.wing = None
-
-    dt = 0.1
 
     boat.set_target(targets[current_target_idx])
 
-    for time_elapsed in np.arange(0, 100, dt):
+    for time_elapsed in np.arange(0, simulation_time, dt):
         if compute_distance(targets[current_target_idx], boat.position) <= target_radius:
             current_target_idx += 1
-            if current_target_idx >= len(targets):
+            if current_target_idx == len(targets):
                 break
             boat.set_target(targets[current_target_idx])
 
-        boat.follow_target(world.wind, dt, simulated_data=True, motor_only=True)
+        boat.follow_target(world.wind, dt, simulated_data, measured_data, motor_only)
+
+        route.append(boat.position)
 
         world.update([boat], dt)
-
-        route.append(np.copy(boat.position))
-        time.append(time_elapsed)
-        if boat.rudder is not None:
-            rudder_angle.append(modpi(boat.rudder.controller.get_angle()))
-            rudder_speed.append(boat.rudder.controller.speed * boat.rudder.controller.direction)
-
-    drawer.draw_axis()
-    drawer.draw_route(route, 'blue')
-    for t in targets:
-        drawer.draw_target(t)
-    drawer.draw_wind(world.wind, np.array([world_width * 0.4, world_height * 0.4]))
-
-    # plt.plot(time, list(map(lambda p: p[0], route)), label='Boat Velocity X')
-    # plt.plot(time, list(map(lambda p: p[1], route)), label='Boat Velocity Y')
-    if len(time) == len(rudder_angle):
-        plt.plot(time, rudder_angle, label='Rudder angle')
-        plt.plot(time, rudder_speed, label='Rudder speed')
-
-    plt.legend()
-    plt.show()
-
-    while True:
-        pass
-
-def simulation_motor_wing(targets):
-
-    current_target_idx = 0
-    target_radius = 5
-    route = []
-    time = []
-    rudder_angle = []
-    rudder_speed = []
-    wing_angle = []
-    wing_speed = []
-
-    boat.position = np.array([0.0, 0.0])
-    boat.velocity = np.array([0.0, 0.0])
-    boat.heading = polar_to_cartesian(1, 0)
-
-    wind.velocity = np.array([12.0, -8.0])
-
-    dt = 0.1
-
-    boat.set_target(targets[current_target_idx])
-
-    for time_elapsed in np.arange(0, 100, dt):
-        if compute_distance(targets[current_target_idx], boat.position) <= target_radius:
-            current_target_idx += 1
-            if current_target_idx >= len(targets):
-                break
-            boat.set_target(targets[current_target_idx])
-
-        boat.follow_target(world.wind, dt, simulated_data=True)
-
-        world.update([boat], dt)
-
-        route.append(np.copy(boat.position))
-        time.append(time_elapsed)
-        if boat.rudder is not None:
-            rudder_angle.append(modpi(boat.rudder.controller.get_angle()))
-            rudder_speed.append(boat.rudder.controller.speed * boat.rudder.controller.direction)
-        if boat.wing is not None:
-            wing_angle.append(modpi(boat.wing.controller.get_angle()))
-            wing_speed.append(boat.wing.controller.speed * boat.wing.controller.direction)
-
-    drawer.draw_axis()
-    drawer.draw_route(route, 'blue')
-    for t in targets:
-        drawer.draw_target(t)
-    drawer.draw_wind(world.wind, np.array([world_width * 0.4, world_height * 0.4]))
-
-    # plt.plot(time, list(map(lambda p: p[0], route)), label='Boat Velocity X')
-    # plt.plot(time, list(map(lambda p: p[1], route)), label='Boat Velocity Y')
-    if len(time) == len(rudder_angle):
-        plt.plot(time, rudder_angle, label='Rudder angle')
-        plt.plot(time, rudder_speed, label='Rudder speed')
     
-    if len(time) == len(wing_angle):
-        plt.plot(time, wing_angle, label='Wing angle')
-        plt.plot(time, wing_speed, label='Wing speed')
-    plt.legend()
-    plt.show()
+    return current_target_idx == len(targets), time_elapsed
 
-    while True:
-        pass
+
+class TestMotorOnly(unittest.TestCase):
+
+    def test_slalom_easy(self):
+
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        ## boat initialization
+        boat = Boat(40, 5, None, Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+
+        targets = [np.array([50, 20]), np.array([100, -20]), np.array([150, 20]), np.array([200, -20])]
+        dt = 0.1
+        simulation_time = 100
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True, motor_only=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 91.6)
+    
+    def test_slalom_medium(self):
+
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        ## boat initialization
+        boat = Boat(40, 5, None, Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+
+        targets = [np.array([50, 50]), np.array([100, -50]), np.array([150, 50]), np.array([200, -50])]
+        dt = 0.1
+        simulation_time = 200
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True, motor_only=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 159.2)
+    
+    def test_sparse(self):
+
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        ## boat initialization
+        boat = Boat(40, 5, None, Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+
+        targets = [np.array([50, 50]), np.array([-100, -50]), np.array([150, 50]), np.array([-200, -50])]
+        dt = 0.1
+        simulation_time = 450
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True, motor_only=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 347.2)
+
+class TestWingMotor(unittest.TestCase):
+
+    def test_slalom_easy(self):
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        wing_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0))
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        wing_area = 8
+        ## boat initialization
+        boat = Boat(40, 5, Wing(wing_area, wing_controller), Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+        
+        wind.velocity = np.array([12.0, -6.0])
+
+        targets = [np.array([50, 20]), np.array([100, -20]), np.array([150, 20]), np.array([200, -20])]
+        dt = 0.1
+        simulation_time = 100
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 70.2)
+    
+    def test_slalom_medium(self):
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        wing_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0))
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        wing_area = 8
+        ## boat initialization
+        boat = Boat(40, 5, Wing(wing_area, wing_controller), Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+        
+        wind.velocity = np.array([12.0, -6.0])
+
+        targets = [np.array([50, 50]), np.array([100, -50]), np.array([150, 50]), np.array([200, -50])]
+        dt = 0.1
+        simulation_time = 150
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 135.5)
+    
+    def test_sparse(self):
+        ## wind initialization
+        wind = Wind(1.291)
+        world = World(9.81, wind)
+
+        rudder_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0), np.pi * 0.15)
+        wing_controller = StepperController(Stepper(100, 0.3), PID(0.5, 0, 0))
+        motor_controller = MotorController(Motor(200, 0.85, 1024))
+
+        wing_area = 8
+        ## boat initialization
+        boat = Boat(40, 5, Wing(wing_area, wing_controller), Rudder(rudder_controller), motor_controller)
+
+        boat.position = np.array([0.0, 0.0])
+        boat.velocity = np.array([0.0, 0.0])
+        boat.heading = polar_to_cartesian(1, 0)
+        
+        wind.velocity = np.array([12.0, -6.0])
+
+        targets = [np.array([50, 50]), np.array([-100, -50]), np.array([150, 50]), np.array([-200, -50])]
+        dt = 0.1
+        simulation_time = 400
+
+        completed, time_elapsed = simulate(boat, world, targets, dt, simulation_time, simulated_data=True)
+
+        self.assertTrue(completed)
+        self.assertAlmostEqual(time_elapsed, 325.4)
+        
+        # current_target_idx = 0
+        # target_radius = 5
+        # dt = 0.1
+        # simulation_time = 100
+        # route = []
+        # time = []
+        # rudder_angle = []
+        # rudder_speed = []
+
+        # boat.position = np.array([0.0, 0.0])
+        # boat.velocity = np.array([0.0, 0.0])
+        # boat.heading = polar_to_cartesian(1, 0)
+        # boat.wing = None
+
+        # boat.set_target(targets[current_target_idx])
+
+        # for time_elapsed in np.arange(0, simulation_time, dt):
+        #     if compute_distance(targets[current_target_idx], boat.position) <= target_radius:
+        #         current_target_idx += 1
+        #         if current_target_idx >= len(targets):
+        #             break
+        #         boat.set_target(targets[current_target_idx])
+
+        #     boat.follow_target(world.wind, dt, simulated_data=True, motor_only=True)
+
+        #     world.update([boat], dt)
+        
+        
+
+    # route.append(np.copy(boat.position))
+    # time.append(time_elapsed)
+    # if boat.rudder is not None:
+    #     rudder_angle.append(modpi(boat.rudder.controller.get_angle()))
+    #     rudder_speed.append(boat.rudder.controller.speed * boat.rudder.controller.direction)
+
+    # drawer.draw_axis()
+    # drawer.draw_route(route, 'blue')
+    # for t in targets:
+    #     drawer.draw_target(t)
+    # drawer.draw_wind(world.wind, np.array([world_width * 0.4, world_height * 0.4]))
+
+    # plt.plot(time, list(map(lambda p: p[0], route)), label='Boat Velocity X')
+    # plt.plot(time, list(map(lambda p: p[1], route)), label='Boat Velocity Y')
+    # if len(time) == len(rudder_angle):
+    #     plt.plot(time, rudder_angle, label='Rudder angle')
+    #     plt.plot(time, rudder_speed, label='Rudder speed')
+
+    # plt.legend()
+    # plt.show()
+
+    # while True:
+    #     pass
+
+# def simulation_motor_wing(targets):
+
+#     current_target_idx = 0
+#     target_radius = 5
+#     route = []
+#     time = []
+#     rudder_angle = []
+#     rudder_speed = []
+#     wing_angle = []
+#     wing_speed = []
+
+#     boat.position = np.array([0.0, 0.0])
+#     boat.velocity = np.array([0.0, 0.0])
+#     boat.heading = polar_to_cartesian(1, 0)
+
+#     wind.velocity = np.array([12.0, -8.0])
+
+#     dt = 0.1
+
+#     boat.set_target(targets[current_target_idx])
+
+#     for time_elapsed in np.arange(0, 500, dt):
+#         if compute_distance(targets[current_target_idx], boat.position) <= target_radius:
+#             current_target_idx += 1
+#             if current_target_idx >= len(targets):
+#                 break
+#             boat.set_target(targets[current_target_idx])
+
+#         boat.follow_target(world.wind, dt, simulated_data=True)
+
+#         world.update([boat], dt)
+
+#         route.append(np.copy(boat.position))
+#         time.append(time_elapsed)
+#         if boat.rudder is not None:
+#             rudder_angle.append(modpi(boat.rudder.controller.get_angle()))
+#             rudder_speed.append(boat.rudder.controller.speed * boat.rudder.controller.direction)
+#         if boat.wing is not None:
+#             wing_angle.append(modpi(boat.wing.controller.get_angle()))
+#             wing_speed.append(boat.wing.controller.speed * boat.wing.controller.direction)
+
+#     drawer.draw_axis()
+#     drawer.draw_route(route, 'blue')
+#     for t in targets:
+#         drawer.draw_target(t)
+#     drawer.draw_wind(world.wind, np.array([world_width * 0.4, world_height * 0.4]))
+
+#     # plt.plot(time, list(map(lambda p: p[0], route)), label='Boat Velocity X')
+#     # plt.plot(time, list(map(lambda p: p[1], route)), label='Boat Velocity Y')
+#     if len(time) == len(rudder_angle):
+#         plt.plot(time, rudder_angle, label='Rudder angle')
+#         plt.plot(time, rudder_speed, label='Rudder speed')
+    
+#     if len(time) == len(wing_angle):
+#         plt.plot(time, wing_angle, label='Wing angle')
+#         plt.plot(time, wing_speed, label='Wing speed')
+#     plt.legend()
+#     plt.show()
+
+#     while True:
+#         pass
 
 if __name__ == '__main__':
+    unittest.main()
+
     # simulation_motor_only([np.array([50, 20]), np.array([100, -20]), np.array([150, 20]), np.array([200, -20])])
     # simulation_motor_only([np.array([50, 50]), np.array([100, -50]), np.array([150, 50]), np.array([200, -50])])
     # simulation_motor_only([np.array([50, 50]), np.array([-100, -50]), np.array([150, 50]), np.array([-200, -50])])
     
-    simulation_motor_wing([np.array([50, 20]), np.array([100, -20]), np.array([150, 20]), np.array([200, -20])])
+    # simulation_motor_wing([np.array([50, 20]), np.array([100, -20]), np.array([150, 20]), np.array([200, -20])])
     # simulation_motor_wing([np.array([50, 50]), np.array([100, -50]), np.array([150, 50]), np.array([200, -50])])
     # simulation_motor_wing([np.array([50, 50]), np.array([-100, -50]), np.array([150, 50]), np.array([-200, -50])])
     
