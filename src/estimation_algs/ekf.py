Update EKF calculations for boat speed variance (bug)

src/estimation_algs/ekf.py
@@ -69,7 +69,7 @@ class EKF:
                 speedometer_par_var,
                 speedometer_perp_var,
                 (1/boat_mass**2) * ((2*wind_speed*np.cos(wind_angle-wing_angle)*np.cos(wind_angle))**2 * anemometer_var  +  (wind_speed**2*(np.sin(wind_angle)*np.cos(wind_angle-wing_angle)+np.cos(wind_angle)*np.sin(wind_angle-wing_angle)))**2 * wing_var  +  (wind_speed**2*np.cos(wind_angle)*np.sin(wind_angle-wing_angle))**2 * anemometerdir_var),
-                (1/boat_mass**2) * ((1/(boat_speed_par+1)**2) * motor_var + (motor_power**2/(boat_speed_par+1)**4) * speedometer_par_var),
+                (1/boat_mass**2) * ((1/(np.abs(boat_speed_par)+1)**2) * motor_var + (motor_power**2/(np.abs(boat_speed_par)+1)**4) * speedometer_par_var),
                 (1/boat_length**2) * ((boat_speed_par**2) / (np.cos(rudder_angle)**4) * rudder_var + np.tan(rudder_angle)**2 * speedometer_par_var)
             ]
         )